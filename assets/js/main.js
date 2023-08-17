53a25402566394b41263761380c6d59d906ac377
Shashikant <shashikantpanchal.info@gmail.com>
2023-08-17 15:09:45 +0530

    Added Models section and made it rensponsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,61 @@
 /*==================== SHOW MENU ====================*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu')
-        })
-    }
+  // Validate that variables exist
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      // We add the show-menu class to the div tag with the nav__menu class
+      nav.classList.toggle('show-menu')
+    })
+  }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
 }
 window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
-    const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+function scrollHeader() {
+  const nav = document.getElementById('header')
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL TOP ====================*/
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
@@ -81,20 +81,20 @@ if (selectedTheme) {
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 2000,
-    reset: true
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+  reset: true
 });
 
 sr.reveal(`.home__data, .home__img,
@@ -103,6 +103,40 @@ sr.reveal(`.home__data, .home__img,
             .app__data, .app__img,
             .contact__data, .contact__button,
             .footer__content`, {
-    interval: 200
+  interval: 200
 })
+
+// <!--========== MAIN JS ==========-->
+const modelCards = document.querySelectorAll('.models__card');
+const modelDetails = document.getElementById('model-details');
+const modelDetailsClose = document.getElementById('model-details-close');
+
+modelCards.forEach((card, index) => {
+  card.addEventListener('click', () => {
+    showModelDetails(index + 1); // Adjust index as needed
+  });
+});
+
+modelDetailsClose.addEventListener('click', () => {
+  hideModelDetails();
+});
+
+function showModelDetails(modelNumber) {
+  const detailsTitle = document.querySelector('.model-details__title');
+  const detailsDescription = document.querySelector('.model-details__description');
+  const detailsImg = document.querySelector('.model-details__img');
+
+  detailsTitle.textContent = `Model ${modelNumber}`;
+  detailsDescription.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et bibendum arcu. Nulla facilisi.'; // Set the model description
+  detailsImg.src = `assets/img/model${modelNumber}-details.jpg`; // Set the image source
+
+  modelDetails.classList.add('show');
+}
+
+function hideModelDetails() {
+  modelDetails.classList.remove('show');
+}
+
+
+
 
